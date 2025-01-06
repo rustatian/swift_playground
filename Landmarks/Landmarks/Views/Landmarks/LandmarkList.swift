beb31aaad227f3f50283f2c8bf2416355e3231ae
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-01-06 22:19:49 +0100

    chore: Landarks test project
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

---
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct LandmarkList: View {
+    @Environment(ModelData.self) var modelData
+    @State private var showFavoritesOnly = false
+
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmarks in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmarks)
+                    } label: {
+                        LandmarkRow(landmark: landmarks)
+                    }
+                }
+            }
+            .animation(.default, value: filteredLandmarks)
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
